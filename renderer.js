644b2344b85f5dc728dcdf3916eb7411427e6e08
tiouoo <tiouo@qq.com>
2026-08-15 02:52:57 +0800

    feat(renderer): 添加动态样式覆盖功能

--- renderer.js
+++ renderer.js
@@ -8,4 +8,20 @@ function onLoad() {
 
     custom_css.rendererReady();
 }
+
+function overrideInline() {
+    let done = false;
+    for (const selector of [".sidebar-nav", ".message-panel"]) {
+        const el = document.querySelector(selector);
+        if (el) {
+            el.style.setProperty("background", "transparent", "important");
+            done = true;
+        }
+    }
+    return done;
+}
+
+const observer = new MutationObserver(() => overrideInline());
+observer.observe(document.documentElement, { childList: true, subtree: true });
+overrideInline();
 onLoad();
